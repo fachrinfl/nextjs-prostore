4a909910149edc93453ec0c0c3c43fe77eeec655
Fachri Naufal <fachrinfl@gmail.com>
2025-01-21 21:31:36 +0700

    initial deployment

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { prisma } from "@/db/prisma";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { convertToPlainObject } from "../utils";
+
+export async function getLatestProducts() {
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return convertToPlainObject(data);
+}
+
+export async function getProductBySlug(slug: string) {
+  console.log("getProductBySlug", slug);
+  return await prisma.product.findFirst({
+    where: {
+      slug,
+    },
+  });
+}
